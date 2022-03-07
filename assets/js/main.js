2c5ed0400551f1fddafbe23c9e126da24b5e7dbc
Arun R Variyath <32630496+arunvariyath@users.noreply.github.com>
2022-03-07 22:00:16 +0530

    updated offset of skill animation and padding of skill section

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,7 +152,7 @@
   if (skilsContent) {
     new Waypoint({
       element: skilsContent,
-      offset: '80%',
+      offset: '98%',
       handler: function(direction) {
         let progress = select('.progress .progress-bar', true);
         progress.forEach((el) => {
